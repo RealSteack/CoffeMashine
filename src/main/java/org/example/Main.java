38589e78a0775dd6d4b3631f00229fab0414025f
artem <artemfoxgames28@gmail.com>
2025-10-26 19:38:40 +0300

    Макет коффе-машины

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.example.displays.Display;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Display display = new Display();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Вы хотите включить кофе машину? :\n" +
+                "           1. Да\n" +
+                "           2. Нет.\n");
+        String input = sc.nextLine();
+
+        switch (input) {
+            case "1":
+                display.displayInfo();
+        }
+
     }
 }
---
+++ src/main/java/org/example/profile/recipes.java
@@ -0,0 +1,6 @@
+package org.example.profile;
+
+public enum recipes {
+    CAPUCINO,
+    ESPRESSO,
+}
